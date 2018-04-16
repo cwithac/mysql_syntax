c4c65a09364a2034393c1a6c4e81468409eaf966
Cathleen Wright <cathleenmwright@gmail.com>
2018-04-16 17:10:48 -0400

    GET functionality for EDIT page

--- addressbook/edit.php
+++ addressbook/edit.php
@@ -1,33 +1,61 @@
 <?php
+session_start();
+
+if(!$_SESSION['loggedInUser']) {
+  header("Location: index.php");
+}
+
+$clientID = $_GET['id'];
+
+include('includes/connection.php');
+include('includes/functions.php');
+
+$query = "SELECT * FROM clients WHERE id='$clientID'";
+$result = mysqli_query($conn, $query);
+
+if(mysqli_num_rows($result) > 0) {
+    while( $row = mysqli_fetch_assoc($result) ) {
+        $clientName     = $row['name'];
+        $clientEmail    = $row['email'];
+        $clientPhone    = $row['phone'];
+        $clientAddress  = $row['address'];
+        $clientCompany  = $row['company'];
+        $clientNotes    = $row['notes'];
+    }
+  } else { // no results returned
+    $alertMessage = "<div class='alert alert-warning'>Nothing to see here. <a href='clients.php'>Head back</a>.</div>";
+  }
+
+
 include('includes/header.php');
 ?>
 
 <h1>Edit Client</h1>
 
-<form action="clients.php" method="post" class="row">
+<form action="<?php echo htmlspecialchars( $_SERVER['PHP_SELF'] ); ?>?id=<?php echo $clientID; ?>" method="post" class="row">
     <div class="form-group col-sm-6">
         <label for="client-name">Name</label>
-        <input type="text" class="form-control input-lg" id="client-name" name="clientName" value="John Doe">
+        <input type="text" class="form-control input-lg" id="client-name" name="clientName" value="<?php echo $clientName; ?>">
     </div>
     <div class="form-group col-sm-6">
         <label for="client-email">Email</label>
-        <input type="text" class="form-control input-lg" id="client-email" name="clientEmail" value="john@email.com">
+        <input type="text" class="form-control input-lg" id="client-email" name="clientEmail" value="<?php echo $clientEmail; ?>">
     </div>
     <div class="form-group col-sm-6">
         <label for="client-phone">Phone</label>
-        <input type="text" class="form-control input-lg" id="client-phone" name="clientPhone" value="(123) 456-7890">
+        <input type="text" class="form-control input-lg" id="client-phone" name="clientPhone" value="<?php echo $clientPhone; ?>">
     </div>
     <div class="form-group col-sm-6">
         <label for="client-address">Address</label>
-        <input type="text" class="form-control input-lg" id="client-address" name="clientAddress" value="111 Address Street, Calgary, AB  T1G 2YK">
+        <input type="text" class="form-control input-lg" id="client-address" name="clientAddress" value="<?php echo $clientAddress; ?>">
     </div>
     <div class="form-group col-sm-6">
         <label for="client-company">Company</label>
-        <input type="text" class="form-control input-lg" id="client-company" name="clientCompany" value="Brightside Studios Inc.">
+        <input type="text" class="form-control input-lg" id="client-company" name="clientCompany" value="<?php echo $clientCompany; ?>">
     </div>
     <div class="form-group col-sm-6">
         <label for="client-notes">Notes</label>
-        <textarea type="text" class="form-control input-lg" id="client-notes" name="clientNotes">Great guy. Usually pays early.</textarea>
+        <textarea type="text" class="form-control input-lg" id="client-notes" name="clientNotes"><?php echo $clientNotes; ?></textarea>
     </div>
     <div class="col-sm-12">
         <hr>
